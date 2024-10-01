6a47ad32d298fac58aa5e4c0d5626ad7d67d9a17
Jarvay <jarvay@163.com>
2024-10-01 15:47:55 +0800

    chore: v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "jarvay.workpaper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1.4"
+        versionCode = 11
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
