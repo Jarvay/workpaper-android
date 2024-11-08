f53524c6d0968b668047acd4b5cb51a3356cf2a7
JiaWei <jarvay@163.com>
2024-11-08 11:42:34 +0800

    chore: v2.0.0-beta.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 13
-        versionName = "2.0.0-beta-2"
+        versionName = "2.0.0-beta.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
