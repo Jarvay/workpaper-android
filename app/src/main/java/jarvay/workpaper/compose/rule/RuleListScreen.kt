ff526b5a19dae53d54f2e0e37397e8d6de6773f8
JiaWei <jarvay@163.com>
2025-05-13 15:50:32 +0800

    feat: quick settings

--- app/src/main/java/jarvay/workpaper/compose/rule/RuleListScreen.kt
+++ app/src/main/java/jarvay/workpaper/compose/rule/RuleListScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import jarvay.workpaper.R
 import jarvay.workpaper.compose.Screen
@@ -49,7 +48,6 @@ import jarvay.workpaper.ui.theme.COLOR_LIST_LABEL
 import jarvay.workpaper.ui.theme.HOME_SCREEN_PAGER_VERTICAL_PADDING
 import jarvay.workpaper.ui.theme.SCREEN_HORIZONTAL_PADDING
 import jarvay.workpaper.viewModel.RuleListViewModel
-import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -242,9 +240,7 @@ private fun RuleItem(
 
                             if (running) {
                                 viewModel.apply {
-                                    viewModelScope.launch {
-                                        workpaper.restart()
-                                    }
+                                    workpaper.restart()
                                 }
                             }
                         }
